501801e12e121a2e4dc154cd16eb56425ece9c00
ganeshgautam <ganeshgautam@192.168.29.81>
2018-01-28 17:40:13 +0530

    Added first cut for ring buffer queues

---
+++ src/main/java/com/ganesh/collections/queue/SimpleRingBufferQueue.java
@@ -0,0 +1,60 @@
+package com.ganesh.collections.queue;
+
+/**
+ * 
+ * @author ganeshgautam
+ * @see <a href = "http://tutorials.jenkov.com/java-performance/ring-buffer.html">ring-buffer</a>
+ *
+ */
+public class SimpleRingBufferQueue {
+    public Object[] elements = null;
+
+    private int capacity  = 0;
+    private int writePos  = 0;
+    private int available = 0;
+
+    public SimpleRingBufferQueue(int capacity) {
+        this.capacity = capacity;
+        this.elements = new Object[capacity];
+    }
+
+    public void reset() {
+        this.writePos = 0;
+        this.available = 0;
+    }
+
+    public int capacity() { return this.capacity; }
+    public int available(){ return this.available; }
+
+    public int remainingCapacity() {
+        return this.capacity - this.available;
+    }
+
+    public boolean put(Object element){
+
+        if(available < capacity){
+            if(writePos >= capacity){
+                writePos = 0;
+            }
+            elements[writePos] = element;
+            writePos++;
+            available++;
+            return true;
+        }
+
+        return false;
+    }
+
+    public Object take() {
+        if(available == 0){
+            return null;
+        }
+        int nextSlot = writePos - available;
+        if(nextSlot < 0){
+            nextSlot += capacity;
+        }
+        Object nextObj = elements[nextSlot];
+        available--;
+        return nextObj;
+    }
+}
