e2913da7a2e6d5e103e168b415cbe3c7d952a3b2
ganeshgautam <ganeshgautam@192.168.29.81>
2018-02-05 22:39:01 +0530

    Added first unit test for the classes

---
+++ src/test/java/com/ganesh/collections/queue/SimpleRingBufferQueueTest.java
@@ -0,0 +1,22 @@
+package com.ganesh.collections.queue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SimpleRingBufferQueueTest {
+	
+	@Before
+	public void setup() {
+	}
+
+	@Test
+	public void put_and_take_normal_single_element() {
+		SimpleRingBufferQueue queue = new SimpleRingBufferQueue(1);
+		Object element = new Object();
+		queue.put(element);
+		Object takenELement = queue.take();
+		Assert.assertEquals(element, takenELement);
+	}
+	
+}
